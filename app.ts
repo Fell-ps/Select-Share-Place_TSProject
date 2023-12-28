46a187807f776d19ce6f6a9729c78de2b71f08b8
Fellps <felps369369@gmail.com>
2023-12-28 08:28:46 -0300

    Creating Search Address API

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log("Your code goes here");
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyBY75zBgtS_nhdE-40jea8zgAUPGDvNJrM";
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+}
+
+form.addEventListener("submit", searchAddressHandler);
